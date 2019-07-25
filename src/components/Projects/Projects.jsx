31580290c233c24e4335726ea58773775c8b768f
Emil0hman <kameragubben04@outlook.com>
2019-07-25 17:16:08 +0200

    Bugfix

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -16,7 +16,7 @@ export default class Projects extends React.Component {
       <div className={"projects"}>
         <Section title="Projects">
           <div className="gridContainer">
-            <ProjectCard title="URL Shortener" img={URLShortener} codeLink="https://github.com/Emil0hman/urlshortener" demoLink="http://emilohman.nu/projects/urlshortener" />
+            <ProjectCard title="URL Shortener" img={URLShortener} codeLink="https://github.com/Emil0hman/url" demoLink="http://emilohman.nu/projects/urlshortener" />
             <ProjectCard title="Clock" img={Clock} codeLink="https://github.com/Emil0hman/clock" demoLink="http://emilohman.nu/projects/clock" />
             <ProjectCard title="Rock, Paper, Scissors" img={RPS} codeLink="https://github.com/Emil0hman/rock-paper-scissors" demoLink="http://emilohman.nu/projects/rps" />
           </div>
